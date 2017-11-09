df6e4b1044671a82cab255e50afc7655e38aaa05
adrianlm98 <32093458+adrianlm98@users.noreply.github.com>
2017-11-09 10:14:19 +0000

    program3.2

--- ut2/a2/program3.py
+++ ut2/a2/program3.py
@@ -1,17 +1,13 @@
 import sys
-
 nota = float(sys.argv[1])
-if nota >=0 or <=10:
-	if nota < 5:
-	print ("Suspenso")
-
-		if nota >= 5 or <= 7:
-	print ("Aprobado")
-
-			if nota >= 9 or <= 10:
-	print ("Sobresaliente")
-
-				if nota = 10:
-	print ("Matrícula")
+if nota >= 0 and nota <= 10:
+  if nota < 5:
+    print("Suspenso")
+  if nota >= 5 and nota <= 7:
+    print("Aprobado")
+  if nota >= 9 and nota <= 10:
+    print("Sobresaliente")
+  if nota == 10:
+    print("Matrícula")
 else:
-	print ("Error introduzca una nota valida")
+    	print("Error introduzca una nota valida")
