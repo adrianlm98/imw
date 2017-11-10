a7fe382e768e36e7f919816187bf7389dbf942fd
adrianlm98 <32093458+adrianlm98@users.noreply.github.com>
2017-11-10 12:27:12 +0000

    final1.0

--- ut2/a2/program4.py
+++ ut2/a2/program4.py
@@ -8,15 +8,19 @@ print("(2) Calcular el perímetro de la circunferencia: p = 2*pi*r")
 print("(3) Calcular el área del círculo: a = pi * r^2")
 print("(4) Salir")
 
-opcion = int(sys.argv[2])
 
-if opcion == 1:
-	print ("Diámetro =", 2 * radio)
-if opcion == 2:
-	print ("Perímetro =", 2 * math.pi * radio)
-if opcion == 3:
-	print ("Área =", math.pi * radio^2)
-if opcion == 4:
-	exit
+opcion = int(input("Elige una opcion:"))
+
+if opcion >=1 and opcion <=4:
+	if opcion == 1:
+		d = 2 *r
+		print ("Diámetro =", d)
+	if opcion == 2:
+		p = 2 * math.pi * radio
+		print ("Perímetro =", 2 * math.pi * radio)
+	if opcion == 3:
+		print ("Área =", math.pi * radio^2)
+	if opcion == 4:
+		exit
 else:
 	print ("Error introduzca una opción valida")
