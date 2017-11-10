48f93dc502f1bd20d933e0a8e3184493cae92939
adrianlm98 <32093458+adrianlm98@users.noreply.github.com>
2017-11-10 10:45:26 +0000

    final1.1

--- ut2/a2/program3.py
+++ ut2/a2/program3.py
@@ -3,9 +3,11 @@ nota = float(sys.argv[1])
 if nota >= 0 and nota <= 10:
   if nota < 5:
     print("Suspenso")
-  if nota >= 5 and nota <= 7:
-    print("Aprobado")
-  if nota >= 9 and nota <= 10:
+  if nota >= 5 and nota < 7:
+    print("Aprobando")
+  if nota >= 7 and nota < 9:
+    print("Notable")
+  if nota >= 9 and nota < 10:
     print("Sobresaliente")
   if nota == 10:
     print("Matrícula")
