636e0c0077e19b7d318038eb03b316ecf464155e
adrianlm98 <32093458+adrianlm98@users.noreply.github.com>
2017-11-27 13:51:10 +0000

    final1.1

--- ut2/a6/program1.py
+++ ut2/a6/program1.py
@@ -1,15 +1,14 @@
 import sys
 
 sentence = sys.argv[1]
-
 def count_words(sentence):
     summary = {}
-    list_words = sentence.split(" ")
-    for char in list_words:
+    listw = sentence.split(" ")
+    for char in listw:
         if char in summary:
             summary[char] += 1
         else:
             summary[char] = 1
     return summary
-for name, number in (count_words(sentence)).items():
-    print(name + ":" + str(number))
+for names, numbers in (count_words(sentence)).items():
+    print(names + ":" + str(numbers))
