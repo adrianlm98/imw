5aa65b1daa8b8035ff4e6c92ac77c5940ff99df8
adrianlm98 <32093458+adrianlm98@users.noreply.github.com>
2017-12-01 17:32:03 +0000

    final1.0

---
+++ ut2/a6/program2.py
@@ -0,0 +1,44 @@
+def add_contacts(phone_book, name, phone):
+    phone_book[name] = phone
+
+def show_contacts(phone_book):
+    if phone_book == {}:
+        print("No hay ningún número añadido")
+    for name, number in phone_book.items():
+        print("{}: {}".format(name, number))
+
+def remove_contact(phone_book, name):
+    del(phone_book[name])
+
+def menu():
+    phone_book = {}
+    while True:
+        print("1. Mostrarme los contactos")
+        print("2. Añadir los contactos")
+        print("3. Eliminar los contactos")
+        print("4. Salir")
+        option = input("Opción: ")
+        if option == "1":
+            show_contacts(phone_book)
+        if option == "2":
+            name = input("Introducir un nombre: ")
+            if name in phone_book:
+                print("El nombre ya existe")
+            else:
+                phone = input("Introduzca un número de teléfono: ")
+                phone_numbers = len(phone)
+                if phone_numbers == 9:
+                    add_contacts(phone_book, name, phone)
+                else:
+                    print("Número incorrecto")
+        if option == "3":
+            name = input("Introduzca el nombre que quiera eliminar : ")
+            if name not in phone_book:
+                print("El nombre no existe")
+            else:
+                remove_contact(phone_book, name)
+        if option == "4":
+            print("Adiós")
+            break
+
+menu()
